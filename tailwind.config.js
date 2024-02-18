d18d8214faeb776081ec71ab54864e4732de24d9
Praveen Singh <pvnsingh05@gmail.com>
2024-02-18 14:25:40 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        "box-shadow": "rgba(136, 165, 191, 0.48) 3px 1px 8px 0px, rgba(136, 165, 191, 0.8) -3px -1px 8px 0px;",
+        "box-shadow": "rgba(136, 165, 191, 0.48) 3px 1px 8px 0px, rgba(136, 165, 191, 0.8) -3px -1px 8px 0px",
         "box-shadow-inside": "inset rgb(204, 219, 232, 0.3) 1px 1px 5px 1px",
         "box-hover-inside": "rgb(0, 0, 0, 0.2) 1px 1px 5px 2px"
       }
